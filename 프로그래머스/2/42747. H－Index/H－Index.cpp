47086f37354aa955dd8520ead485934476a475be
zeus <94885018+Jung-jaewoo@users.noreply.github.com>
2024-03-27 21:28:20 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,9 +1,14 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
-    return answer;
+    sort(citations.begin(),citations.end(),greater<>());
+    for(int i=0; i<citations.size(); i++){
+        if(citations[i]<i+1)
+            return i;
+    }
+    return citations.size();
 }
