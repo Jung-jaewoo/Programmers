141fa2257be420909d59b23bb6bb4707576b8996
zeus <94885018+Jung-jaewoo@users.noreply.github.com>
2024-10-03 16:41:26 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    my_string.resize(n);
+    answer = my_string;
+    return answer;
+}
