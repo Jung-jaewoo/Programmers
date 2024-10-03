f57085a522867b317a5373808cc069747f9f9629
zeus <94885018+Jung-jaewoo@users.noreply.github.com>
2024-10-03 18:29:57 +0900

    [level 2] Title: N-Queen, Time: 153.91 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<pair<int,int>> points;
+int sum = 0;
+
+bool checkPoint(int x, int y){
+    for(int i=0; i<points.size(); i++){
+        if(x == points[i].first || y == points[i].second)
+            return false;
+        if(x + y == points[i].first + points[i].second)
+            return false;
+        if(x - y == points[i].first - points[i].second)
+            return false;
+    }
+    return true;
+}
+
+void putQueen(int& n, int x){
+    if(n == x) {
+        sum++;
+        return;
+    }
+    
+    for(int i=0; i<n; i++){
+        if(checkPoint(x,i)){
+            points.push_back(make_pair(x,i));
+            putQueen(n, x + 1);
+            points.pop_back();
+        }
+    }
+}
+
+int solution(int n) {
+    putQueen(n,0);
+    return sum;
+}
