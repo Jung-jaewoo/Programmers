8810f529beb403f6431b99404cb804f40b9464c9
zeus <94885018+Jung-jaewoo@users.noreply.github.com>
2024-10-04 17:07:39 +0900

    [level 2] Title: 올바른 괄호, Time: 0.40 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -7,18 +7,15 @@ using namespace std;
 bool solution(string s)
 {
     bool answer = true;
-    stack<char> stk;
+    int n = 0;
     for(int i=0; i<s.size(); i++){
-        if(s[i] == '('){
-            stk.push('(');
-        }
+        if(s[i] == '(')
+            n++;
         else{
-            if(stk.size()==0)
-                return false;
-            stk.pop();
+            if(n==0) return false;
+            n--;
         }
     }
-    if(stk.size()!=0)
-        return false;
+    if(n!=0) return false;
     return answer;
 }
